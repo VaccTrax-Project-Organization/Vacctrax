a2d93ad41ab1d91bdfa498ca72dc78a535bbddd3
Anmoldeep Singh Gill <gillasg788@gmail.com>
2021-03-17 19:40:39 -0400

    Added basic Homepage and Updated .gitignore

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {HomepageComponent} from './pages/homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppMaterialModule} from './angular.material';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
