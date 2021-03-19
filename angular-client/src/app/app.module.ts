b5287253e45fc7c2224f00ba222eb08e92fd7854
Razei <razeixyz@gmail.com>
2021-03-19 16:11:27 -0400

    Added patient appointment component

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppMaterialModule} from './angular.material';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ViewAppointmentsComponent } from './pages/patient/view-appointments/view-appointments.component';
+import { AppointmentComponent } from './pages/patient/appointment/appointment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    ViewAppointmentsComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
