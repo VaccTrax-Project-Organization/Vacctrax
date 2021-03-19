daf5c2c4e85781c65d5a71478ce716ba586b5c4c
Razei <razeixyz@gmail.com>
2021-03-19 16:12:31 -0400

    Fixed error

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppMaterialModule} from './angular.material';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { ViewAppointmentsComponent } from './pages/patient/view-appointments/view-appointments.component';
 import { AppointmentComponent } from './pages/patient/appointment/appointment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    ViewAppointmentsComponent,
     AppointmentComponent
   ],
   imports: [
