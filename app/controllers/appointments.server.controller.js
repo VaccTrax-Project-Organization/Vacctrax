44fb8fd38c138e216f945b3966a4d576c5ba56ec
Anmoldeep Singh Gill <gillasg788@gmail.com>
2021-03-20 16:57:34 -0400

    modified seed data files to validate Object Ids

--- app/controllers/appointments.server.controller.js
+++ app/controllers/appointments.server.controller.js
@@ -1,2 +1,19 @@
 const Appointment = require('mongoose').model('Appointment');
 
+exports.requestAppointment = (req, res) => {
+    console.log(req.body);
+}
+
+exports.getAllAppointmentsForClinic = (req, res, next) => {
+    const clinic = req.clinic;
+
+    Appointment.find({clinic: clinic}, (err, appointments) => {
+        if (err) {
+            return next(err);
+        } else {
+            console.log(appointments);
+            // getting appointment list
+            res.status(200).send(appointments);
+        }
+    });
+}
