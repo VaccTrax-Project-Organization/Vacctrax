9ca9d22bda546f8843425865aeb812cb3f76e1e9
Razei <razeixyz@gmail.com>
2021-03-22 22:47:54 -0400

    Revert "Merge remote-tracking branch 'origin/main' into main"

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
 import {HomepageComponent} from './pages/homepage/homepage.component';
-import {AppointmentComponent} from "./shared/appointment/appointment.component";
 
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
-  { path: 'appointment', component: AppointmentComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -6,15 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppMaterialModule} from './angular.material';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { AppointmentComponent } from './shared/appointment/appointment.component';
-import { NavigationBarComponent } from './shared/navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    AppointmentComponent,
-    NavigationBarComponent
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
