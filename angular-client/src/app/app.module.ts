270ad9308b4ecba2d98bcfe8bdd293004bf93584
Razei <razeixyz@gmail.com>
2021-03-22 22:36:43 -0400

    Shared Navigation bar and appointment components

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppMaterialModule} from './angular.material';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AppointmentComponent } from './shared/appointment/appointment.component';
+import { NavigationBarComponent } from './shared/navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    AppointmentComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
