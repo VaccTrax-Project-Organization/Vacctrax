cdbc968f6c823fe30e9ae1e7058fd12fe7066059
Razei <razeixyz@gmail.com>
2021-03-24 18:01:15 -0400

    Moved shared pages folder into pages

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppMaterialModule} from './angular.material';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { AppointmentComponent } from './shared/appointment/appointment.component';
-import { NavigationBarComponent } from './shared/navigation-bar/navigation-bar.component';
+import { AppointmentComponent } from './pages/shared/appointment/appointment.component';
+import { NavigationBarComponent } from './pages/shared/navigation-bar/navigation-bar.component';
 import {MaterialAngularModule} from './material-angular.module';
 
 @NgModule({
