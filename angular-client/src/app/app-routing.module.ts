d808afb6bbe01cc5ec3ce867b70482d9419f90e3
Asad Mahmood <asadmahmood1995@gmail.com>
2021-03-24 14:25:44 -0400

    Angular Version Fix, re-implemented PatientContainer

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {AppComponent} from './app.component';
 import {HomepageComponent} from './pages/homepage/homepage.component';
-import {AppointmentComponent} from "./shared/appointment/appointment.component";
+import {AppointmentComponent} from './shared/appointment/appointment.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: 'appointment', component: AppointmentComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
+  {path: '', component: HomepageComponent},
+  {path: 'appointment', component: AppointmentComponent},
+  {
+    path: 'patient',
+    loadChildren: () => import('./pages/patient-container/patient-container.module').then(m => m.PatientContainerModule)
+  },
+  {path: '**', redirectTo: '', pathMatch: 'full'},
 
 ];
 
@@ -16,4 +20,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
