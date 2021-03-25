ceb9798cf0ea775d5e8d254c8433af2772f956fc
Razei <razeixyz@gmail.com>
2021-03-25 17:12:06 -0400

    Updated spec to for mat-table

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {AppMaterialModule} from './angular.material';
+import { AppMaterialModule } from './angular.material';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { AppointmentComponent } from './pages/shared/appointment/appointment.component';
 import { NavigationBarComponent } from './pages/shared/navigation-bar/navigation-bar.component';
-import {MaterialAngularModule} from './material-angular.module';
+import { MaterialAngularModule } from './material-angular.module';
 import { GenericTwoOptionDialogComponent } from './pages/shared/generic-two-option-dialog/generic-two-option-dialog.component';
 
 @NgModule({
