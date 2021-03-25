b5be41632949c8a67d28a6e9985e75e16335f99e
Razei <razeixyz@gmail.com>
2021-03-25 11:19:33 -0400

    Added console log with swagger page link

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.listen(3000);
 
 // Log the server status to the console
 console.log("Server running at http://localhost:3000/");
+console.log("Swagger at http://localhost:3000/api-docs");
 
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app;
