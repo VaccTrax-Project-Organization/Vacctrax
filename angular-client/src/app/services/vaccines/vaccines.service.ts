1a3d07a0d4879fa342e5aca22d353a0ff0fe0dfb
Razei <razeixyz@gmail.com>
2021-03-27 00:37:21 -0400

    Update vaccine to use environment url

--- angular-client/src/app/services/vaccines/vaccines.service.ts
+++ angular-client/src/app/services/vaccines/vaccines.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../../environments/environment";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from 'rxjs';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,6 @@ export class VaccinesService {
   constructor(private http: HttpClient) { }
 
   public getVaccines(): Observable<any> {
-    return this.http.get<any>(`http://localhost:3000/api/vaccines`, {headers: this.httpHeader, withCredentials: true})
+    return this.http.get<any>(`${this.url}/vaccines`, {headers: this.httpHeader, withCredentials: true})
   }
 }
