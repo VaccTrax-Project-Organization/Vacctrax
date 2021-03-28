ca97f68795a25e4f8c5fe6c1eaa1d4641f661333
Razei <razeixyz@gmail.com>
2021-03-28 19:41:10 -0400

    small fixes

--- angular-client/src/app/models/healthPractitioner.model.ts
+++ angular-client/src/app/models/healthPractitioner.model.ts
@@ -1,7 +1,9 @@
 import {Appointment} from './appointment.model';
 import {Account} from './account.class';
+import {Clinic} from './clinic.model';
 
 export class HealthPractitioner extends Account {
   upcomingAppointments: Appointment[];
   account: Account;
+  clinic: Clinic;
 }
