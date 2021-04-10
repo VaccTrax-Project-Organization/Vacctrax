f47a251cddc121ecce5185d35c620224818e3bb3
Razei <razeixyz@gmail.com>
2021-04-10 19:09:37 -0400

    Made signup button navigate to signup page

--- angular-client/src/app/pages/homepage/homepage.component.ts
+++ angular-client/src/app/pages/homepage/homepage.component.ts
@@ -3,6 +3,7 @@ import {MatDialog} from '@angular/material/dialog';
 import {SubSink} from 'subsink';
 import {GenericTwoOptionDialogComponent} from '../../shared/pages/generic-two-option-dialog/generic-two-option-dialog.component';
 import {GenericTwoOptionDialogData} from '../../models/generic-two-option-dialog-data';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-homepage',
@@ -12,7 +13,7 @@ import {GenericTwoOptionDialogData} from '../../models/generic-two-option-dialog
 export class HomepageComponent implements OnInit, OnDestroy {
   private subSink: SubSink;
 
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog, private router: Router) {
     this.subSink = new SubSink();
   }
 
@@ -25,21 +26,7 @@ export class HomepageComponent implements OnInit, OnDestroy {
 
   // this method should not be within the homepage, its temporary for until we
   // have the appointment cancellation stuff ready
-  public openCancelVaccinationDialog(): void {
-    const dialogTitle = 'CANCEL APPOINTMENT';
-    const dialogDescription = 'Are you sure you would like to cancel the selected appointment (enter appoint number here or something), this action cannot be undone';
-    const dialogRef = this.dialog.open(GenericTwoOptionDialogComponent, {
-      panelClass: 'dialog-panel-class',
-      width: '650px',
-      height: '350px',
-      disableClose: true,
-      autoFocus: false,
-      data: new GenericTwoOptionDialogData(dialogTitle, dialogDescription)
-    });
-
-    // get call back data on close
-    this.subSink.add(dialogRef.afterClosed().subscribe(res => {
-      console.log('after close callback', res);
-    }));
+  public navigateToSignUp(): void {
+    this.router.navigateByUrl('/signup').catch(err => console.log(err));
   }
 }
