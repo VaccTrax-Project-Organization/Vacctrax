0027e52984b9b29f50039cc677518af8981a4c6f
Asad Mahmood <asadmahmood1995@gmail.com>
2021-04-12 11:36:41 -0400

    Created SetPasswordComponent

---
+++ angular-client/src/app/pages/set-password/set-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set-password',
+  templateUrl: './set-password.component.html',
+  styleUrls: ['./set-password.component.scss']
+})
+export class SetPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
