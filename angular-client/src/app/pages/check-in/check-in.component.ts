74be80e6f6fcb3dff552d85cacf640c2dd24cc6f
Alvin <asjyap29@hotmail.com>
2021-04-11 23:36:48 -0400

    Completed  Integration Of PUT appoint completed and decline

--- angular-client/src/app/pages/check-in/check-in.component.ts
+++ angular-client/src/app/pages/check-in/check-in.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { inject } from '@angular/core/testing';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Appointment } from 'src/app/models/appointment.model';
+import { AppointmentType } from 'src/app/models/enums/appointment.enum';
+import { AppointmentService } from 'src/app/services/appointment/appointment.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-check-in',
   templateUrl: './check-in.component.html',
   styleUrls: ['./check-in.component.scss']
 })
-export class CheckInComponent implements OnInit {
+export class CheckInComponent implements OnInit, OnDestroy {
   checkInForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  subSink: SubSink;
+  constructor(private formBuilder: FormBuilder, private appointmentService: AppointmentService, 
+     @Inject(MAT_DIALOG_DATA) private appointment: Appointment,) {
+       this.subSink = new SubSink();
+      }
+  ngOnDestroy(): void {
+    this.subSink.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.checkInForm = this.createCheckInForm();
@@ -26,6 +39,8 @@ export class CheckInComponent implements OnInit {
   }
 
   checkInPatient($event) {
-
+    this.appointment.type = AppointmentType.CONFIRMED;
+    this.subSink.add(this.appointmentService.updateAppointment(this.appointment).subscribe(res=>{console.log(res)}));
+      
   }
 }
