363e22a9c686586e53ae37e05263df1a3855b92a
Anmoldeep Singh Gill <gillasg788@gmail.com>
2021-04-18 01:39:27 -0400

    Added more unit tests

--- angular-client/src/app/services/appointment/appointment.service.spec.ts
+++ angular-client/src/app/services/appointment/appointment.service.spec.ts
@@ -1,10 +1,14 @@
 import {async, inject, TestBed} from '@angular/core/testing';
 
 import { AppointmentService } from './appointment.service';
-import {PatientService} from "../patient/patient.service";
-import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
-import {environment} from "../../../environments/environment";
-import {AppointmentType} from "../../models/enums/appointment.enum";
+import {PatientService} from '../patient/patient.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {environment} from '../../../environments/environment';
+import {AppointmentType} from '../../models/enums/appointment.enum';
+import {Vaccine} from "../../models/vaccine.model";
+import {Clinic} from "../../models/clinic.model";
+import {Patient} from "../../models/patient.model";
+import {HealthPractitioner} from "../../models/healthPractitioner.model";
 
 describe('AppointmentService', () => {
   let appointmentService: AppointmentService;
@@ -33,16 +37,16 @@ describe('AppointmentService', () => {
 
       appointService.requestAppointment({
         reason: 'Unit test appointment',
-        preferredDate: '',
-        preferredTime: '',
-        vaccine: '',
-        vaccineDose: '',
+        preferredDate: '2021-03-28T20:48:45.535+00:00',
+        preferredTime: '2021-03-28T12:30:00.535+00:00',
+        vaccine: '6060e7d900130baf8c5b6a70',
+        vaccineDose: '2nd',
         type: AppointmentType.REQUESTED,
-        clinicId: '',
-        patientId: '',
+        clinicId: '6060e0d489d72d6c899a6284',
+        patientId: '6060df17c0edd45cd49d2f57',
       })
         .subscribe((result: any) => {
-          expect(result._id).toBe('');
+          expect(result._id).toBe('6060edfa00b639ea04d96fe2');
         });
 
       // @ts-ignore
@@ -51,15 +55,56 @@ describe('AppointmentService', () => {
 
       req.flush(
         {
-          _id: '',
+          _id: '6060edfa00b639ea04d96fe2',
           reason: 'Unit test appointment',
-          preferredDate: '',
-          preferredTime: '',
-          vaccine: '',
-          vaccineDose: '',
+          preferredDate: '2021-03-28T20:48:45.535+00:00',
+          preferredTime: '2021-03-28T12:30:00.535+00:00',
+          vaccine: '6060e7d900130baf8c5b6a70',
+          vaccineDose: '2nd',
           type: AppointmentType.REQUESTED,
-          clinicId: '',
-          patientId: '',
+          clinic: '6060e0d489d72d6c899a6284',
+          patient: '6060df17c0edd45cd49d2f57',
+        });
+      httpMock.verify();
+
+    })));
+
+  it(`could cancel appointment`, async(inject([HttpTestingController, AppointmentService],
+    (httpClient: HttpTestingController, appointService: AppointmentService) => {
+
+      appointService.cancelAppointment({
+        _id: '6060ee5300b639ea04d96fe2',
+        reason: 'Unit test appointment',
+        preferredDate: new Date('2021-03-28T20:48:45.535+00:00'),
+        preferredTime: new Date('2021-03-28T12:30:00.535+00:00'),
+        vaccine: new Vaccine('6060e7d900130baf8c5b6a70'),
+        vaccineDose: '2nd',
+        type: AppointmentType.REQUESTED,
+        clinic: new Clinic('6060e0d489d72d6c899a6284'),
+        patient: new Patient('6060df17c0edd45cd49d2f57'),
+        startTime: new Date('2021-03-28T20:48:45.535+00:00'),
+        endTime: new Date('2021-03-28T20:48:45.535+00:00'),
+        healthPractitioner: new HealthPractitioner('6060e4a0e8efb27ed157ad1b')
+      })
+        .subscribe((result: any) => {
+          expect(result.type).toBe(AppointmentType.CANCELLED);
+        });
+
+      // @ts-ignore
+      const req = httpMock.expectOne(environment.apiUrl + '/appointments/' + '6060ee5300b639ea04d96fe2');
+      expect(req.request.method).toBe('PUT');
+
+      req.flush(
+        {
+          _id: '6060edfa00b639ea04d96fe2',
+          reason: 'Unit test appointment',
+          preferredDate: '2021-03-28T20:48:45.535+00:00',
+          preferredTime: '2021-03-28T12:30:00.535+00:00',
+          vaccine: '6060e7d900130baf8c5b6a70',
+          vaccineDose: '2nd',
+          type: AppointmentType.CANCELLED,
+          clinic: '6060e0d489d72d6c899a6284',
+          patient: '6060df17c0edd45cd49d2f57',
         });
       httpMock.verify();
 
