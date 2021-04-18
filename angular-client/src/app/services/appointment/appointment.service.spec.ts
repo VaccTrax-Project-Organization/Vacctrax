aefc7c6efbfac99a7f561c13807e04bf85083f6b
Anmoldeep Singh Gill <gillasg788@gmail.com>
2021-04-17 21:52:33 -0400

    Added request appointment service unit test

--- angular-client/src/app/services/appointment/appointment.service.spec.ts
+++ angular-client/src/app/services/appointment/appointment.service.spec.ts
@@ -1,16 +1,67 @@
-import { TestBed } from '@angular/core/testing';
+import {async, inject, TestBed} from '@angular/core/testing';
 
 import { AppointmentService } from './appointment.service';
+import {PatientService} from "../patient/patient.service";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {environment} from "../../../environments/environment";
+import {AppointmentType} from "../../models/enums/appointment.enum";
 
 describe('AppointmentService', () => {
-  let service: AppointmentService;
+  let appointmentService: AppointmentService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AppointmentService);
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        AppointmentService
+      ],
+    });
+
+    appointmentService = TestBed.inject(AppointmentService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(appointmentService).toBeTruthy();
   });
+
+  it(`could request appointment`, async(inject([HttpTestingController, AppointmentService],
+    (httpClient: HttpTestingController, appointService: AppointmentService) => {
+
+      appointService.requestAppointment({
+        reason: 'Unit test appointment',
+        preferredDate: '',
+        preferredTime: '',
+        vaccine: '',
+        vaccineDose: '',
+        type: AppointmentType.REQUESTED,
+        clinicId: '',
+        patientId: '',
+      })
+        .subscribe((result: any) => {
+          expect(result._id).toBe('');
+        });
+
+      // @ts-ignore
+      const req = httpMock.expectOne(environment.apiUrl + '/requestAppointment');
+      expect(req.request.method).toBe('POST');
+
+      req.flush(
+        {
+          _id: '',
+          reason: 'Unit test appointment',
+          preferredDate: '',
+          preferredTime: '',
+          vaccine: '',
+          vaccineDose: '',
+          type: AppointmentType.REQUESTED,
+          clinicId: '',
+          patientId: '',
+        });
+      httpMock.verify();
+
+    })));
 });
