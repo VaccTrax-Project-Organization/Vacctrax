219b6b900e0252ad438f9f66680bc22e5d72c942
Razei <razeixyz@gmail.com>
2021-09-30 08:30:35 -0400

    Test webhook

--- angular-client/src/app/services/vaccines/vaccines.service.ts
+++ angular-client/src/app/services/vaccines/vaccines.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
 import {Service} from '../service.class';
 import {Vaccine} from '../../models/vaccine.model';
 import {catchError} from 'rxjs/operators';
